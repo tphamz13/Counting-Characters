99236bf76c2cfab83ef9cb049d1a1c80222c0b44
tphamz13 <tienpham_vu@yahoo.com>
2023-03-11 15:55:46 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        string strFirst;
+        char charCount;
+        int Count = 0;
+        Console.Write("Enter your text: ");
+        strFirst = Console.ReadLine();
+        Console.Write("Enter Count Character: ");
+        charCount = Convert.ToChar(Console.ReadLine());
+        Console.Write("Your character Count: ");
+        foreach(char chr in strFirst) {
+            if (chr == charCount) {
+                Count++;
+            }
+        }
+        Console.Write(Count);
+    }
+}
